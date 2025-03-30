8852ffaa2677a75c0220d79643d2a714a59be37e
paulooosf <phpessoal2021@gmail.com>
2025-03-30 11:57:24 -0300

    feat: incluindo link para inicio na header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
 import Logo from '../../assets/logo.png'
 import Pesquisa from '../Pesquisa/Pesquisa'
-import { slide as MenuHamburguer } from 'react-burger-menu'
 import './Header.css'
 import './MenuHamburguer.css'
+import { Link } from 'react-router-dom'
 
 // TODO: Alterar exibição de links caso o usuário esteja deslogado e caso seja um admin.
 
 function Header() {
   return (
-    <div className="container">
-      <MenuHamburguer>
-        <a href="">INÍCIO</a>
-        <a href="">EM ALTA</a>
-        <a href="">SAIR</a>
-      </MenuHamburguer>
+    <div className="header__container">
       <header>
           <img src={Logo} alt="Logotipo do GameboXed" className="header__logo" />
           <Pesquisa/>
           <ul className="header__links">
-            <li><a href="">INÍCIO</a></li>
+            <li>
+              <Link to="/">INÍCIO</Link>
+            </li>
             <li><a href="">EM ALTA</a></li>
             <li><a href="">SAIR</a></li>
           </ul>
