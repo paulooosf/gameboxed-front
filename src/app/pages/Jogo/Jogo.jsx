580c71da35270e1b55cc014a54002322592e6e48
paulooosf <phpessoal2021@gmail.com>
2025-03-30 11:58:07 -0300

    feat: finalizando página de jogo

--- src/app/pages/Jogo/Jogo.jsx
+++ src/app/pages/Jogo/Jogo.jsx
@@ -10,11 +10,11 @@ function Jogo() {
     <body className="jogo">
         <Header/>
         <div className="jogo__background">
-            <img src="https://pbs.twimg.com/media/GGonApTWkAApY-n.jpg:large" alt="" className="jogo__background__imagem"/>
+            <img src="https://pbs.twimg.com/media/GGonApTWkAApY-n.jpg:large" alt="Banner do jogo" className="jogo__background__imagem"/>
         </div>
         <article className="jogo__sobre">
             <aside className="jogo__sobre__capa">
-                <img src="https://image.api.playstation.com/vulcan/ap/rnd/202411/2217/15e82cd2b2f42ee8bcb56a37703da3cc722d42805e8eeddd.png" alt="" className="jogo__sobre__capa__imagem"/>
+                <img src="https://image.api.playstation.com/vulcan/ap/rnd/202411/2217/15e82cd2b2f42ee8bcb56a37703da3cc722d42805e8eeddd.png" alt="Capa do jogo" className="jogo__sobre__capa__imagem"/>
             </aside>
             <section className="jogo__sobre__info">
                 <div className="jogo__sobre__info__nome">
@@ -36,13 +36,25 @@ function Jogo() {
             </section>
         </article>
         <article className="jogo__avaliacoes">
-            <p className="jogo__avaliacoes__tipografia">Avaliações recentes:</p>
-            <div className="jogo__avaliacoes__card">
-                <Avaliacao/>
+            <div className="jogo__avaliacoes__tipografias">
+                <p className="jogo__avaliacoes__tipografia__recentes">Avaliações recentes:</p>
+                <button className="jogo__avaliacoes__tipografia__ver__mais">Ver mais</button>
+            </div>
+            <div className="jogo__avaliacoes__cards">
+                <Avaliacao
+                    titulo="Avaliação de racemaster123:"
+                    avaliacao="Que jogo incrível!! Passei horas fazendo diversas corridas e nem vi o tempo passar."
+                    nota="5"
+                />
+                <Avaliacao
+                    titulo="Avaliação de racemaster123:"
+                    avaliacao="Que jogo incrível!! Passei horas fazendo diversas corridas e nem vi o tempo passar."
+                    nota="5"
+                />
             </div>
         </article>
         <article className="jogo__trailer">
-
+            <iframe width="589" height="431" src="https://www.youtube.com/embed/ugKIELPN8mI?si=7DsfPZOV9P4mH4fV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </article>
         <Footer/>
     </body>
