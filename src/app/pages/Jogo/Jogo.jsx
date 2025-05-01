175945249cce42b37a19e09419f277a2ca4198f9
Paulo Henrique <phpessoal2021@gmail.com>
2025-05-01 20:33:41 -0300

    feat: integrando página de jogos à API

--- src/app/pages/Jogo/Jogo.jsx
+++ src/app/pages/Jogo/Jogo.jsx
@@ -1,37 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Jogo.css'
 import Header from '../../../components/Header/Header'
 import Footer from '../../../components/Footer/Footer'
 import Avaliacao from '../../../components/Avaliacao/Avaliacao'
 import { FaStar } from 'react-icons/fa'
+import { useParams } from 'react-router-dom'
+import { getJogo } from '../../../api/jogos'
+import { toast } from 'react-toastify'
+import { useLogin } from '../../context/LoginContext'
 
 function Jogo() {
+    const { id } = useParams()
+    const { logado } = useLogin()
+    const [jogo, setJogo] = useState({})
+
+    useEffect(() => {
+        const carregarDados = async () => {
+            try {
+                const resposta = await getJogo(id)
+                setJogo(resposta)
+            } catch (erro) {
+                toast.error('Erro ao buscar dados do jogo.')
+                console.error('Erro na requisição de dados do jogo: ' + erro)
+            }
+        }
+
+        carregarDados()
+    }, [])
+
   return (
     <div className="jogo">
-        <Header modo="logado"/>
+        <Header modo={logado ? 'logado' : 'deslogado'}/>
         <div className="jogo__background">
-            <img src="https://pbs.twimg.com/media/GGonApTWkAApY-n.jpg:large" alt="Banner do jogo" className="jogo__background__imagem"/>
+            <img src={jogo.linkBanner} alt="Banner do jogo" className="jogo__background__imagem"/>
         </div>
         <article className="jogo__sobre">
             <aside className="jogo__sobre__capa">
-                <img src="https://image.api.playstation.com/vulcan/ap/rnd/202411/2217/15e82cd2b2f42ee8bcb56a37703da3cc722d42805e8eeddd.png" alt="Capa do jogo" className="jogo__sobre__capa__imagem"/>
+                <img src={jogo.linkCapa} alt="Capa do jogo" className="jogo__sobre__capa__imagem"/>
             </aside>
             <section className="jogo__sobre__info">
                 <div className="jogo__sobre__info__nome">
-                    <p className="jogo__sobre__info__nome__titulo">The Crew Motorfest</p>
-                    <p className="jogo__sobre__info__nome__ano">2024</p>
+                    <p className="jogo__sobre__info__nome__titulo">{jogo.nome}</p>
+                    <p className="jogo__sobre__info__nome__ano">{jogo.ano}</p>
                 </div>
-                <p className="jogo__sobre__info__empresa">Ubisoft</p>
-                <p className="jogo__sobre__info__descricao">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit vitae facilis error enim placeat provident ad velit, ex dolorem, unde nihil aliquam molestias? Perferendis repellendus facilis a dolores est iusto.</p>
+                <p className="jogo__sobre__info__empresa">{jogo.empresa}</p>
+                <p className="jogo__sobre__info__descricao">{jogo.descricao}</p>
             </section>
             <section className="jogo__sobre__avaliacoes">
                 <div className="jogo__sobre__avaliacoes__nota">
-                    <p className="jogo__sobre__avaliacoes__nota__tipografia">4.75</p>
+                    <p className="jogo__sobre__avaliacoes__nota__tipografia">{parseFloat(jogo.nota).toFixed(2)}</p>
                     <div className="jogo__sobre__avaliacoes__nota__estrela">
                         <FaStar/>
                     </div>
                 </div>
-                <p className="jogo__sobre__avaliacoes__quantidade">200 avaliações</p>
+                <p className="jogo__sobre__avaliacoes__quantidade">{jogo.quantidadeAvaliacoes === 1 ? jogo.quantidadeAvaliacoes + ' Avaliação' : jogo.quantidadeAvaliacoes + ' Avaliações'}</p>
                 <button className="jogo__sobre__avaliacoes__botao__avaliar">Avaliar</button>
             </section>
         </article>
@@ -54,11 +76,30 @@ function Jogo() {
             </div>
         </article>
         <article className="jogo__trailer">
-            <iframe width="589" height="431" src="https://www.youtube.com/embed/ugKIELPN8mI?si=7DsfPZOV9P4mH4fV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            {jogo.linkTrailer && (
+                <iframe width="589" height="431" src={converterLinkYoutube(jogo.linkTrailer)} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            )}
         </article>
         <Footer/>
     </div>
   )
 }
+
+function converterLinkYoutube(link) {
+    try {
+      const url = new URL(link)
+  
+      if (url.hostname === 'youtu.be') {
+        const videoId = url.pathname.slice(1)
+        return `https://www.youtube.com/embed/${videoId}`
+      }
+  
+      const idVideo = url.searchParams.get('v')
+      return idVideo ? `https://www.youtube.com/embed/${idVideo}` : null
+    } catch (erro) {
+      console.error('Erro ao converter o link do trailer: ' + erro)
+      return null
+    }
+  }
 
 export default Jogo
