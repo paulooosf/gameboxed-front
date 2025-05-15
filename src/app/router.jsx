746f8d16b95284892748786fb107094828d5a2eb
Paulo Henrique <phpessoal2021@gmail.com>
2025-05-14 23:12:23 -0300

    feat: criação da página de jogos e integração com a busca

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -3,6 +3,7 @@ import Inicio from "./pages/Inicio/Inicio.jsx"
 import Jogo from "./pages/Jogo/Jogo.jsx"
 import Registro from "./pages/Registro/Registro.jsx"
 import Login from "./pages/Login/Login.jsx"
+import Jogos from './pages/Jogos/Jogos.jsx'
 
 function AppRouter() {
     return (
@@ -11,6 +12,7 @@ function AppRouter() {
             <Route path="/jogo/:id" element={<Jogo/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/registro" element={<Registro/>}/>
+            <Route path="/jogos" element={<Jogos/>}/>
         </Routes>
     )
 }
