501e0ed360eb9e168b8c5070df581b1aaf4ba46d
Paulo Henrique <phpessoal2021@gmail.com>
2025-05-14 23:11:14 -0300

    feat: permitindo busca no serviço de listar jogos

--- src/api/jogos.js
+++ src/api/jogos.js
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const API_URL = 'http://localhost:8080'
 
-export const getJogos = async (pagina, tamanhoLista, ordenacao) => {
-    const resposta = await axios.get(`${API_URL}/jogos/lista?page=${pagina}&size=${tamanhoLista}&sort=${ordenacao}`)
+export const getJogos = async (pagina, tamanhoLista, ordenacao, nomeJogo) => {
+    const resposta = await axios.get(`${API_URL}/jogos/lista`, {
+        params: {
+            page: pagina,
+            size: tamanhoLista,
+            sort: ordenacao,
+            busca: nomeJogo
+        }
+    })
 
     return resposta.data
 }
